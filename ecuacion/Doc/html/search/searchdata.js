dc90c15571b1c37574db6885e463d1d775a11bdd
SaidBe11 <saidbecerril1010@gmail.com>
2020-12-02 21:00:11 -0600

    Documentacion

---
+++ ecuacion/Doc/html/search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "bcgmr",
+  1: "cgm",
+  2: "gm",
+  3: "bmr"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "functions",
+  3: "variables"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Files",
+  2: "Functions",
+  3: "Variables"
+};
+
